a50a468da6bcb4244aa40bbcf1f54bd355bcf12e
Masato Kikukawa <kikukawa9@gmail.com>
2023-12-31 02:52:58 +0900

    command memo

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def callback():
     signature = request.headers['X-Line-Signature']
 
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    app.logger.info(body)
 
     try:
         handler.handle(body, signature)
