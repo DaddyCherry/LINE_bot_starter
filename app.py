cbae4eb3b2a7f4cc49845415f138292065a2ceee
Masato Kikukawa <60773422+kikukawa9@users.noreply.github.com>
2024-01-13 16:48:27 +0900

    hoge

--- app.py
+++ app.py
@@ -38,9 +38,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    print(event.reply_token)
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=event.message.text+event.user_id))
 
 
 if __name__ == "__main__":
