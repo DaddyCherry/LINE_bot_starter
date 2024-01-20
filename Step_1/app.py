d754d8b47830f60268f5cba867ec5bcb18b62b07
Masato Kikukawa <kikukawa9@gmail.com>
2024-01-20 18:08:56 +0900

    hoge

--- Step_1/app.py
+++ Step_1/app.py
@@ -9,42 +9,42 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!!'
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!!'
 
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
 
-# YOUR_CHANNEL_ACCESS_TOKEN = os.getenv('CHANNEL_ACCESS_TOKEN')
-# YOUR_CHANNEL_SECRET = os.getenv('CHANNEL_SECRET')
+YOUR_CHANNEL_ACCESS_TOKEN = os.getenv('CHANNEL_ACCESS_TOKEN')
+YOUR_CHANNEL_SECRET = os.getenv('CHANNEL_SECRET')
 
-# line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-# handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
-# @app.route("/callback", methods=['POST'])
-# def callback():
-#     signature = request.headers['X-Line-Signature']
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
 
-#     body = request.get_data(as_text=True)
-#     app.logger.info(body)
+    body = request.get_data(as_text=True)
+    app.logger.info(body)
 
-#     try:
-#         handler.handle(body, signature)
-#     except InvalidSignatureError:
-#         abort(400)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
-#     return 'OK'
-
-
-# @handler.add(MessageEvent, message=TextMessage)
-# def handle_message(event):
-#     print(event.reply_token)
-
-#     line_bot_api.reply_message(
-#         event.reply_token,
-#         TextSendMessage(text=event.message.text+line_bot_api.get_profile(event.source.user_id).user_id))
+    return 'OK'
 
 
-# if __name__ == "__main__":
-#    app.run()
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    print(event.reply_token)
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text+line_bot_api.get_profile(event.source.user_id).user_id))
+
+
+if __name__ == "__main__":
+   app.run()
