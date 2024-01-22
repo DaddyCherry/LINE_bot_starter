7dae3a91e44a360d478abf5d55a941ce23f0a201
Masato Kikukawa <kikukawa9@gmail.com>
2024-01-23 01:14:43 +0900

    hoge

--- Step_2/azure_blob_handler.py
+++ Step_2/azure_blob_handler.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 from azure.data.tables import TableServiceClient
 from azure.data.tables import TableEntity
@@ -12,19 +13,36 @@ connect_str = os.getenv("AZURE_STORAGE_CONNECT_STR")
 # テーブルサービスクライアントの作成
 table_service = TableServiceClient.from_connection_string(conn_str=connect_str)
 
-# 使用するテーブル名
-table_name = "YourTableName"
 
-# テーブルが存在しない場合は作成
-table_client = table_service.create_table_if_not_exists(table_name=table_name)
+def write_entity_to_shift_table(emp_id, rec_type):
 
-# 書き込むエンティティの作成
-entity = TableEntity()
-entity['PartitionKey'] = 'sample_partition'
-entity['RowKey'] = 'sample_row_01'
-entity['SampleProperty'] = 'Sample Value'
+    # 使用するテーブル名
+    table_name = "ShiftRecords"
 
-# エンティティをテーブルに書き込む
-table_client.upsert_entity(entity=entity)
+    # テーブルが存在しない場合は作成
+    table_client = table_service.create_table_if_not_exists(table_name=table_name)
 
-print("Entity successfully written to the table.")
+    # 書き込むエンティティの作成
+    entity = TableEntity()
+    entity['PartitionKey'] = emp_id
+    entity['RowKey'] = emp_id + '_' + rec_type
+    entity['Date'] = datetime.datetime.now().strftime('%Y-%m-%d')
+    entity['Time'] = datetime.datetime.now().strftime('%H:%M:%S')
+    entity['RecType'] = rec_type
+
+    # エンティティをテーブルに書き込む
+    table_client.upsert_entity(entity=entity)
+    print("Entity successfully written to the table.")
+
+
+
+def main():
+    write_entity_to_shift_table('emp_0001', 'IN')
+    write_entity_to_shift_table('emp_0001', 'OUT')
+    write_entity_to_shift_table('emp_0002', 'IN')
+    write_entity_to_shift_table('emp_0002', 'OUT')
+    pass
+
+
+if __name__ == "__main__":
+   main()
